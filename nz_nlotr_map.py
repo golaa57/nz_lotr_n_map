6bf35bc83d60299b6d29c0d3a5a2314adcf9b8f2
golaa57 <antek.gola11@gmail.com>
2026-05-18 22:26:06 +0200

    lotr locations

--- nz_nlotr_map.py
+++ nz_nlotr_map.py
@@ -47,6 +47,152 @@ pois = [
         ]
     },
     {
+        "name": "Ścieżka umarłych - Pūtangirua Pinnacles",
+        "lat": -41.4507,
+        "lon": 175.2223,
+        "category": "lotr",
+        "description": "Aragorn idzie do króla umarłych",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/death_path_1.jpg",
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/death_path_2.jpg"
+        ]
+    },
+    {
+        "name": "Ogrody Isengardu - Harcourt Park",
+        "lat": -41.1020,
+        "lon": 175.0960,
+        "category": "lotr",
+        "description": "Ogrody, w których rozmawiają Gandalf z Sarumanem",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/isengard_g_1.jpg",
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/isengard_g_2.jpg"
+        ]
+    },
+    {
+        "name": "Glenorchy",
+        "lat": -44.85,
+        "lon": 168.3833333333,
+        "category": "both",
+        "description": "Różne sceny z LOTRa i Narnii. Ciężko mi znaleźć jakieś konkretne ujęcia, ale to jest po prostu ładna okolica",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/glenorchy_1.jpg"
+        ]
+    },
+    {
+        "name": "Rzeka Dart",
+        "lat": -44.8466666667,
+        "lon": 168.3647222222,
+        "category": "both",
+        "description": "Rzeka z LOTRa i Narnii. Sceny z Kaspiana czy Drużyny Pierścienia",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/dart_1.jpg"
+        ]
+    },
+    {
+        "name": "Nen Hithoel - jeziora Mavora",
+        "lat": -45.2555555556,
+        "lon": 168.1680555556,
+        "category": "lotr",
+        "description": "Rzeka z LOTRa. Pływają po niej w drużynie pierścienia",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/mavora_1.jpg",
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/mavora_2.jpg"
+        ]
+    },
+    {
+        "name": "Queenstown",
+        "lat": -45.0311111111,
+        "lon": 168.6625,
+        "category": "lotr",
+        "description": "Różne sceny z LOTRa. Ładnie",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/queen_1.jpg",
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/queen_2.jpg"
+        ]
+    },
+    {
+        "name": "Edoras - Mount Sunday",
+        "lat": -43.5480555556,
+        "lon": 170.8930555556,
+        "category": "lotr",
+        "description": "Edoras, czyli państwo koników. Głównie w drugiej części",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/edoras_1.jpg",
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/edoras_2.jpg"
+        ]
+    },
+    {
+        "name": "Pola Pelennoru - Twizel",
+        "lat": -44.25,
+        "lon": 170.1,
+        "category": "lotr",
+        "description": "Bitwa z trzeciej części",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/pelennor_1.jpg"
+        ]
+    },
+    {
+        "name": "Jezioro Pukaki",
+        "lat": -44.1166666667,
+        "lon": 170.1666666667,
+        "category": "lotr",
+        "description": "Bliżej nieokreślone ujęcia. Też ładnie",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/pukaki_1.jpg"
+        ]
+    },
+    {
+        "name": "Argonath i Fangorn - Fiordland National Park",
+        "lat": -45.4283305556,
+        "lon": 167.3622888889,
+        "category": "lotr",
+        "description": "Las entów z drugiej części. Rzeka z pierwszej części",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/fiordland_1.jpg",
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/fiordland_2.jpg"
+        ]
+    },
+    {
+        "name": "Rzeka - Kawarau Gorge",
+        "lat": -45.02,
+        "lon": 169.09,
+        "category": "lotr",
+        "description": "Rzeka z pierwszej części",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/kawa_1.jpg"
+        ]
+    },
+    {
+        "name": "Remarkables",
+        "lat": -45.0719444444,
+        "lon": 168.8080555556,
+        "category": "lotr",
+        "description": "Bliżej nieokreślone ujęcia z LOTRa/Hobbita",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/remark_1.jpg"
+        ]
+    },
+    {
+        "name": "Mount Aspiring National Park",
+        "lat": -44.3833333333,
+        "lon": 168.7333333333,
+        "category": "lotr",
+        "description": "Różne z LOTRa. Wydaję mi się, że np. ujęcia z przekazywaniem sygnału w trzeciej części",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/aspi_1.jpg"
+        ]
+    },
+    {
+        "name": "Rohan - Canterbury Plains",
+        "lat": -43.64,
+        "lon": 172.09,
+        "category": "lotr",
+        "description": "Pono Rohan, ale to jest duży region",
+        "images": [
+            "https://raw.githubusercontent.com/golaa57/nz_lotr_n_map/main/images/rohan_1.jpg"
+        ]
+    },
+    {
         "name": "Narnia Forest",
         "lat": -43.532,
         "lon": 172.636,
@@ -64,10 +210,13 @@ styles = {
         "color": "green",
         "icon": "tree"
     },
-
     "narnia": {
         "color": "red",
         "icon": "crown"
+    },
+    "both": {
+        "color": "blue",
+        "icon": "castle"
     }
 }
 
